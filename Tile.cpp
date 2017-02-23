d88ef01788b7b6c35eea63e05adbcd5af47c35c7
KyleSp <kspurloc@umich.edu>
2017-02-22 23:34:47 -0500

    Updated Tile

--- Tile.cpp
+++ Tile.cpp
@@ -1,2 +1,27 @@
+//Kyle Spurlock
+
 #include "Tile.h"
 
+Tile::Tile(int row, int col, int type) : row(row), col(col), val(type) {
+	val = 0;
+}
+
+int Tile::getRow() {
+	return row;
+}
+
+int Tile::getCol() {
+	return col;
+}
+
+int Tile::getVal() {
+	return val;
+}
+
+int Tile::getType() {
+	return type;
+}
+
+void Tile::setVal(int val) {
+	this->val = val;
+}
--- Tile.h
+++ Tile.h
@@ -1,13 +1,23 @@
+//Kyle Spurlock
+
 #ifndef TILE_H
 #define TILE_H
 
 class Tile {
 public:
+	Tile(int row, int col, int type);
 	
+	int getRow();
+	int getCol();
+	int getVal();
+	int getType();
+	
+	void setVal(int val);
 private:
-	int row;
-	int col;
-	int val;
+	int row;			//row number on map
+	int col;			//column number on map
+	int type;			//corresponds to tile type (TODO: maybe make 4 booleans for edges?)
+	int val;			//value assigned for traversing maze
 };
 
 #endif
